6ff0b33f97afa0cdfbc0bb5bf0c82bb0248c3eb1
DjordjeVukmir <lanicor91chef@gmail.com>
2025-05-07 16:05:48 +0200

    drag and drop, 24

---
+++ tests/dragAndDrop.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('Mouse drag and drop', async ({ page }) => {
+
+    await page.goto('http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
+
+
+
+
+    const rome = await page.locator('#box6')
+    const italy = await page.locator('#box106')
+    //1
+    /*await rome.hover()
+    await page.mouse.down()
+    await italy.hover()
+    await page.mouse.up()
+
+    await page.waitForTimeout(3000)
+    */
+    //2
+
+    await rome.dragTo(italy)
+    await page.waitForTimeout(3000)
+
+    const washington = await page.locator('#box3')
+    const usa = await page.locator('#box103')
+
+    await washington.dragTo(usa)
+    await page.waitForTimeout(3000)
+
+})
